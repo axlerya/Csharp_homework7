f5e80148a825d3ccf891cb8f57c05d24011cd02d
axlerya <white_cat_02@mail.ru>
2023-08-27 16:30:13 +0300

    feat: add task 2
    add a solution to the second task

---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+
+int[,] matrix = new int[m, n];
+
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = rand.Next(-100, 100);
+    }
+}
+
+Console.WriteLine("введите номер строки");
+int numRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количномерество столбца");
+int numColumn = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    Console.WriteLine("эмелент с позицией " + "row:" + numRow + " column:" + numColumn +  " = " + matrix[numRow, numColumn]); 
+}
+catch 
+{
+    Console.WriteLine("такой позиции нет"); 
+}
