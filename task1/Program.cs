976961241403b7453dff3fa291e84d3baf0257f1
axlerya <white_cat_02@mail.ru>
2023-08-27 16:29:55 +0300

    feat: add task 1
    add a solution to the first task

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+
+double[,] matrix = new double[m, n];
+
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = rand.Next(-100, 100) + rand.NextDouble();
+    }
+}
+
+void printArray(double[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write(matrix[rows, columns] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+printArray(matrix);
